7166fa9936bb46f9dc84260c2222ef84f5315cd6
Uriel Mata Castellanos <urielmata37@hotmail.com>
2023-11-01 17:26:44 -0600

    XD

--- JS/carrusel.js
+++ JS/carrusel.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
-    var slider1 = $("#slider1");
-    var slider2 = $("#slider2");
-    
+    let slider1 = $("#slider1");
+    let slider2 = $("#slider2");
+    let sig = $("#btn-der");
+    let ant = $("#btn-izq");
+    let n_img = 2;
+    let i = 0;
+
+    sig.click(function () {
+        slider1.animate({left: "-100%"}, 400)
+        slider2.animate({left: "0"}, 400)
+    })
+
+    ant.click(function () {
+        slider1.animate({left: "0"}, 400)
+        slider2.animate({left: "100%"}, 400)
+    })
 
 })
