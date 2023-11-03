62b11e2e5976d184a51e4bef66a1ff59c95b82aa
Uriel Mata Castellanos <urielmata37@hotmail.com>
2023-11-03 11:32:40 -0600

    XD

--- JS/carrusel.js
+++ JS/carrusel.js
@@ -1,19 +1,39 @@
 $(document).ready(function () {
     let slider1 = $("#slider1");
     let slider2 = $("#slider2");
-    let sig = $("#btn-der");
-    let ant = $("#btn-izq");
-    let n_img = 2;
-    let i = 0;
+    let f_der = $("#btn-der");
+    let f_izq = $("#btn-izq");
+    let b_der = $("#btn-bolita-der");
+    let b_izq = $("#btn-bolita-izq");
 
-    sig.click(function () {
+    function sig() {
         slider1.animate({left: "-100%"}, 400)
         slider2.animate({left: "0"}, 400)
-    })
+        b_der.addClass("bol-selec")
+        b_izq.removeClass("bol-selec")
+    }
 
-    ant.click(function () {
+    function ant() {
         slider1.animate({left: "0"}, 400)
         slider2.animate({left: "100%"}, 400)
+        b_der.removeClass("bol-selec")
+        b_izq.addClass("bol-selec")
+    }
+
+    f_der.click(function () {
+        sig();
+    })
+
+    f_izq.click(function () {
+        ant();
+    })
+
+    b_der.click(function () {
+        sig();
+    })
+
+    b_izq.click(function () {
+        ant();
     })
 
 })
