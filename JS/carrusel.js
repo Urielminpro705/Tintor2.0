7b107bf98fae91aec0b37d502af6fa515a8665cc
Urielminpro705 <114453823+Urielminpro705@users.noreply.github.com>
2023-11-07 00:41:15 +0000

    XD

--- JS/carrusel.js
+++ JS/carrusel.js
@@ -5,19 +5,50 @@ $(document).ready(function () {
     let f_izq = $("#btn-izq");
     let b_der = $("#btn-bolita-der");
     let b_izq = $("#btn-bolita-izq");
+    let pos = 0;
 
     function sig() {
-        slider1.animate({left: "-100%"}, 400)
-        slider2.animate({left: "0"}, 400)
-        b_der.addClass("bol-selec")
-        b_izq.removeClass("bol-selec")
+        if (pos == 0) {
+            slider1.animate({ left: "-100%" }, 400, function () {
+                slider1.css("left", "100%");
+            })
+            slider2.animate({ left: "0" }, 400)
+            b_der.addClass("bol-selec")
+            b_izq.removeClass("bol-selec")
+            pos = 1;
+
+        } else {
+            if (pos == 1) {
+                slider1.animate({ left: "0" }, 400)
+                slider2.animate({ left: "-100%" }, 400, function () {
+                    slider2.css("left", "100%");
+                })
+                b_der.removeClass("bol-selec")
+                b_izq.addClass("bol-selec")
+                pos = 0;
+            }
+        }
     }
 
     function ant() {
-        slider1.animate({left: "0"}, 400)
-        slider2.animate({left: "100%"}, 400)
-        b_der.removeClass("bol-selec")
-        b_izq.addClass("bol-selec")
+        if (pos == 0) {
+            slider2.css("left", "-100%");
+            slider1.animate({ left: "100%" }, 400)
+            slider2.animate({ left: "0" }, 400)
+            b_der.addClass("bol-selec")
+            b_izq.removeClass("bol-selec")
+            pos = 1;
+        } else {
+            if (pos == 1) {
+                slider1.css("left", "-100%");
+                slider1.animate({ left: "0" }, 400)
+                slider2.animate({ left: "100%" }, 400)
+                b_der.removeClass("bol-selec")
+                b_izq.addClass("bol-selec")
+                pos = 0;
+            }
+        }
+
     }
 
     f_der.click(function () {
